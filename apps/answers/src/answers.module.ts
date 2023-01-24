0af6b1e3bec4841c2d0d36bdc045c7637cd53a9e
dprastha <danielprastha4@gmail.com>
2023-01-24 20:58:05 +0700

    fix db module

--- apps/answers/src/answers.module.ts
+++ apps/answers/src/answers.module.ts
@@ -9,6 +9,10 @@ import { Answer } from '../entities/answer.entity';
     TypeOrmModule.forFeature([Answer]),
     TypeOrmModule.forRoot({
       type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'root',
       database: 'nestjs_db_answers',
       entities: [Answer],
       synchronize: true,
--- apps/questions/src/questions.module.ts
+++ apps/questions/src/questions.module.ts
@@ -9,6 +9,10 @@ import { QuestionsService } from './questions.service';
     TypeOrmModule.forFeature([Question]),
     TypeOrmModule.forRoot({
       type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'root',
       database: 'nestjs_db_questions',
       entities: [Question],
       synchronize: true,
